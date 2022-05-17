11bc1903444c3a80c2026b2440bbc04962b864e6
kijeong69 <kijeong1996@gmail.com>
2022-05-17 21:44:07 +0900

    unit25

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <input 
+      class="form-control" type="text" 
+      v-model="searchText" placeholder="Search"
+    >
+    <hr>
+    <!-- 자식Component에서 부모Component 
+         @자식의emit명="부모의함수명"-->
+    <!-- 부모Component에서 자식Component 
+         :자식에서사용될이름="부모에서넘겨줄자료"-->
+    <TodoSimpleForm @add-todo="addTodo" />    
+    <div v-if="!filteredTodos.length">There is nothing to display</div>   
+    <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" /> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TodoSimpleForm,
+    TodoList
+  },
+
+  setup() {
+    const todos = ref([]);    
+
+    const todoStyle = {
+      textDecoration: 'line-through',
+      color: 'gray'
+    };
+
+    const addTodo = (todo) => {
+      console.log(todo);
+      todos.value.push(todo);
+    };
+
+    const toggleTodo = (index) => {
+      console.log(todos.value[index]);
+      todos.value[index].completed = !todos.value[index].completed;
+      console.log(todos.value[index]);
+    };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };    
+
+    const searchText = ref('');
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      } else {
+        return todos.value;
+      }
+    });
+
+    return {
+      todos,
+      todoStyle,
+      addTodo,
+      toggleTodo,
+      deleteTodo,
+      searchText,
+      filteredTodos,
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
